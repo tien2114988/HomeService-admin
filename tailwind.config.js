7fddc506478edc50b2df7045bbe61aad10a9f521
Tien Nguyen <tiennguyen2283@gmail.com>
2024-12-14 22:07:10 +0700

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: 'jit',
@@ -24,5 +26,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
